42e4afae6c82c58fc9198490934bf78acc2bfdb1
mahdibagheri <bagheri.almas@gmail.com>
2023-07-22 21:41:29 +0330

    add static files of template

--- cms/settings.py
+++ cms/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from .local_settings import *
 
 # Application definition
@@ -71,6 +73,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'statics')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
