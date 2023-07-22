4d3a1995aac1056268f10bf581e7b749c8c5b811
mahdibagheri <bagheri.almas@gmail.com>
2023-07-22 18:07:16 +0330

    change the app name from posts to blogs

--- cms/settings.py
+++ cms/settings.py
@@ -9,7 +9,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'posts.apps.PostsConfig',
+    'blog.apps.BlogsConfig',
 ]
 
 MIDDLEWARE = [
