f627b243d5c469347b4fb42ccc4ca2618109f0ca
mahdibagheri <bagheri.almas@gmail.com>
2023-07-24 21:47:05 +0330

    change views from functions to class-base

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import list_view, detail_view
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', list_view, name='list_view'),
-    path('<int:year>/<int:month>/<int:day>/<str:slug>/',detail_view, name='detail_view')
+    # path('', list_view, name='list_view'),
+    path('', PostListView.as_view()),
+    path('<int:year>/<int:month>/<int:day>/<str:slug>/',PostDetailView.as_view(), name='detail_view')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
 
 # Create your views here.
-def list_view(request):
-    posts = Post.objects.filter(status='published', publish_time__lte=timezone.now())
-    context = {
-        'posts': posts
-    }
-    return render(request, 'blog/list.html', context)
+# def list_view(request):
+#     posts = Post.objects.filter(status='published', publish_time__lte=timezone.now())
+#     context = {
+#         'posts': posts
+#     }
+#     return render(request, 'blog/list.html', context)
 
 
-def detail_view(request, year, month, day, slug):
-    post = get_object_or_404(Post, status='published', publish_time__year=year, publish_time__month=month,
-                             publish_time__day=day, slug=slug)
-    context = {
-        'post': post
-    }
-    return render(request, 'blog/detail.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/list.html'
+    context_object_name = 'posts'
+
+
+# def detail_view(request, year, month, day, slug):
+#     post = get_object_or_404(Post, status='published', publish_time__year=year, publish_time__month=month,
+#                              publish_time__day=day, slug=slug)
+#     context = {
+#         'post': post
+#     }
+#     return render(request, 'blog/detail.html', context)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        return Post.objects.get(status='published', publish_time__year=self.kwargs['year'],
+                                publish_time__month=self.kwargs['month'], publish_time__day=self.kwargs['day'],
+                                slug=self.kwargs['slug'])
