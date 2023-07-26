43ed8759b418ec4ae7bd8f5c245af3da5a2b190b
mahdibagheri <bagheri.almas@gmail.com>
2023-07-26 17:42:13 +0330

    fix: Correct form inheritance in CommentForm
    - Change inheritance from forms.Form to forms.ModelForm

--- blog/forms.py
+++ blog/forms.py
@@ -3,8 +3,7 @@ from django import forms
 from .models import Comment
 
 
-class CommentForm(forms.Form):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['created', 'updated', 'approved', 'post']
- 
